c4e9b6af71b39a0968fb747724c872d08e83e9bc
shengweixu <shengweixu@outlook.com>
2020-02-23 15:03:29 +0800

    complete github authorization

--- src/main/java/dev/willsnow/community/controller/AuthorizeController.java
+++ src/main/java/dev/willsnow/community/controller/AuthorizeController.java
@@ -1,5 +1,10 @@
 package dev.willsnow.community.controller;
 
+import dev.willsnow.community.dto.AccessTokenDTO;
+import dev.willsnow.community.dto.GithubUser;
+import dev.willsnow.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +16,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeController {
 
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientID;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirectURL;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url(redirectURL);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
         return "index";
     }
 }
+
