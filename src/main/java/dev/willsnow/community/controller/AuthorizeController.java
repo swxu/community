e7fe961893b12e49d8beafb93f0216f3eb36a376
shengweixu <shengweixu@outlook.com>
2020-02-25 01:28:57 +0800

    keep user's state

--- src/main/java/dev/willsnow/community/controller/AuthorizeController.java
+++ src/main/java/dev/willsnow/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author will
  */
@@ -30,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -38,9 +41,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_url(redirectURL);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser.getName());
-        return "index";
+        GithubUser user = githubProvider.getUser(accessToken);
+
+        if (user != null) {
+            /*
+            Login successfully, write cookie and session.
+             */
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            /*
+            Failed to log in, re-login
+             */
+            return "redirect:/";
+        }
+
     }
 }
 
