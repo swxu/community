7ba76b9bf6d6868c5b86382791d6ba774cdccccd
shengweixu <shengweixu@outlook.com>
2020-02-27 02:30:55 +0800

    add flyway migaration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,12 @@
+create table USER
+(
+    ID           INT auto_increment,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(20),
+    TOKEN        CHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+    constraint USER_PK
+        primary key (ID)
+);
+
