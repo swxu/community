c68418a19a6f8e06332821bc6d151d62b5e24bb5
shengweixu <shengweixu@outlook.com>
2020-03-07 01:40:28 +0800

    add comment logistic control

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,7 +15,15 @@ function post() {
             if (response.code === 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code === 2003) {
+                    let isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=85cfd8da29006b0b77db&redirect_uri=http://localhost:8999/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },
